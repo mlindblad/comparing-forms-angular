ffdc809d8d79fd8fceeeb111ba20bf5fde18c8db
mlindblad <martin.lindblad@gmail.com>
2020-03-10 15:51:20 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-algp5a
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-algp5a)
---
+++ src/app/simple-form/simple-form.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'simple-form',
+  templateUrl: './name-editor.component.html',
+  styleUrls: ['./name-editor.component.css']
+})
+export class SimpleFormComponent {
+  name = 'Simple Form'
+
+  
+}
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
