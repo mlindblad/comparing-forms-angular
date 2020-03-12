1f7838f2692b8ea0b1b81b6d97014b4af38e92f4
martin.lindblad <martin.lindblad@polystar.com>
2020-03-12 08:52:56 +0100

    Simple form ready

--- src/app/simple-form/simple-form.component.ts
+++ src/app/simple-form/simple-form.component.ts
@@ -8,4 +8,47 @@ import { FormControl } from '@angular/forms';
 })
 export class SimpleFormComponent {
   title = 'Simple Form'
+  
+  name = '';
+  invalidNameErrorMessage = '';
+  
+  email = '';
+  invalidEmailErrorMessage = '';
+  submitMessage = ''
+
+  successMessage = '';
+
+  validateName() {
+    this.invalidNameErrorMessage = '';
+  
+    if (this.name.length < 3) {
+      this.invalidNameErrorMessage = 'Name must be at least 3 characters long'
+    }
+
+    if (this.name === '') {
+      this.invalidNameErrorMessage = 'Name is required'
+    }
+  }
+
+  validateEmail() {
+    this.invalidEmailErrorMessage = '';
+
+    if (this.email.indexOf('@') === -1) {
+      this.invalidEmailErrorMessage = 'Must contain @ sign';
+    }
+  }
+
+  validateOnSubmit() {
+    this.submitMessage = '';
+    this.successMessage = '';
+    this.validateName();
+    this.validateEmail();
+    if (this.invalidNameErrorMessage || this.invalidEmailErrorMessage) {
+      this.submitMessage = 'some fields contain invalid information';
+    }
+
+    if (this.submitMessage === '') {
+      this.successMessage = 'Hurray. Ok to send form';
+    }
+  }
 }
